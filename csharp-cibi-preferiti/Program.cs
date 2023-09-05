a80e276512c4e935b409b0f2d912a169fd8b7aee
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-09-05 13:37:26 +0200

    first-commit

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//variables
+string[] favoriteFoods = { "Pasta al forno", "Fettuccine al ragù", "Lasagna", "Pizza", "Pollo allo spiedo", "Bistecca", "Panini"};
+//code
+Console.WriteLine("Ecco una classifica dei miei " + favoriteFoods.Length + " cibi preferiti");
+
+for (int i = 0; i < favoriteFoods.Length; i++)
+{
+    Console.WriteLine((i+1) + ") " + favoriteFoods[i]);
+}
+
+Console.WriteLine("Al primo posto: " + favoriteFoods[0]);
+Console.WriteLine("In ultima posizione: " + favoriteFoods[favoriteFoods.Length - 1]);
+
