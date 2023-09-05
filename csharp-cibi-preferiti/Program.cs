0559c0d2c07affd35687aaef0ae4d4a758743818
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-09-05 14:49:19 +0200

    added bonus

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,5 +1,8 @@
 ﻿//variables
 string[] favoriteFoods = { "Pasta al forno", "Fettuccine al ragù", "Lasagna", "Pizza", "Pollo allo spiedo", "Bistecca", "Panini"};
+int numberOfFoods = favoriteFoods.Length;
+
+
 //code
 Console.WriteLine("Ecco una classifica dei miei " + favoriteFoods.Length + " cibi preferiti");
 
@@ -9,5 +12,19 @@ for (int i = 0; i < favoriteFoods.Length; i++)
 }
 
 Console.WriteLine("Al primo posto: " + favoriteFoods[0]);
-Console.WriteLine("In ultima posizione: " + favoriteFoods[favoriteFoods.Length - 1]);
+
+
+//se l'array è dispari, voglio stampare la posizione / 2
+if (favoriteFoods.Length % 2 == 0)
+{
+    Console.WriteLine("Al centro: 404 not found");
+
+}
+else
+{
+Console.WriteLine("Al centro: " + favoriteFoods[numberOfFoods / 2]);
+
+}
+
+Console.WriteLine("In ultima posizione: " + favoriteFoods[numberOfFoods - 1]);
 
