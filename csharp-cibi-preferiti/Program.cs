828e1a5ffc563359cab73e7b7f496d01fb1a5fe9
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-09-05 15:45:08 +0200

    refactor if - else for center food message

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,6 +1,7 @@
 ﻿//variables
 string[] favoriteFoods = { "Pasta al forno", "Fettuccine al ragù", "Lasagna", "Pollo allo spiedo", "Bistecca", "Panini"};
 int numberOfFoods = favoriteFoods.Length;
+var message = "";
 
 
 //code
@@ -11,19 +12,19 @@ for (int i = 0; i < favoriteFoods.Length; i++)
     Console.WriteLine((i+1) + ") " + favoriteFoods[i]);
 }
 
-Console.WriteLine("Al primo posto: " + favoriteFoods[0]);
-
-
 //se l'array è dispari, voglio stampare la posizione / 2
 if (favoriteFoods.Length % 2 == 0)
 {
-    Console.WriteLine("Al centro: " + favoriteFoods[(numberOfFoods / 2) - 1] + " e " + favoriteFoods[numberOfFoods / 2]);
+    message = "Al centro: " + favoriteFoods[(numberOfFoods / 2) - 1] + " e " + favoriteFoods[numberOfFoods / 2];
 }
 else
 {
-Console.WriteLine("Al centro: " + favoriteFoods[numberOfFoods / 2]);
-
+    message = "Al centro: " + favoriteFoods[numberOfFoods / 2];
 }
+
+Console.WriteLine("Al primo posto: " + favoriteFoods[0]);
+
+Console.WriteLine(message);
 
 Console.WriteLine("In ultima posizione: " + favoriteFoods[numberOfFoods - 1]);
 
