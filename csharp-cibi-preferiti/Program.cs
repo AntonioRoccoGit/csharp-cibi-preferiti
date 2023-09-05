f3b5a85822005af5d9313e4afd4945a6ec869b2b
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-09-05 15:03:08 +0200

    added case, when favorite foods are odd

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,10 +1,10 @@
 ﻿//variables
-string[] favoriteFoods = { "Pasta al forno", "Fettuccine al ragù", "Lasagna", "Pizza", "Pollo allo spiedo", "Bistecca", "Panini"};
+string[] favoriteFoods = { "Pasta al forno", "Fettuccine al ragù", "Lasagna", "Pollo allo spiedo", "Bistecca", "Panini"};
 int numberOfFoods = favoriteFoods.Length;
 
 
 //code
-Console.WriteLine("Ecco una classifica dei miei " + favoriteFoods.Length + " cibi preferiti");
+Console.WriteLine("Ecco una classifica dei miei " + numberOfFoods + " cibi preferiti");
 
 for (int i = 0; i < favoriteFoods.Length; i++)
 {
@@ -17,8 +17,7 @@ Console.WriteLine("Al primo posto: " + favoriteFoods[0]);
 //se l'array è dispari, voglio stampare la posizione / 2
 if (favoriteFoods.Length % 2 == 0)
 {
-    Console.WriteLine("Al centro: 404 not found");
-
+    Console.WriteLine("Al centro: " + favoriteFoods[(numberOfFoods / 2) - 1] + " e " + favoriteFoods[numberOfFoods / 2]);
 }
 else
 {
