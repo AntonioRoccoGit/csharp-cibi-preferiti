3b8084b1e8fef8f2fd13cda6ecced94492dbb980
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-09-05 16:34:58 +0200

    refactor code

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,26 +1,29 @@
 ﻿//variables
-string[] favoriteFoods = { "Pasta al forno", "Fettuccine al ragù", "Lasagna", "Pollo allo spiedo", "Bistecca", "Panini"};
+string[] favoriteFoods = { "Pasta al forno", "Fettuccine al ragù", "Lasagna", "Pollo allo spiedo", "Bistecca", "Panini" };
 int numberOfFoods = favoriteFoods.Length;
+var message = $"Al centro: ";
 
 //code
-Console.WriteLine("Ecco una classifica dei miei " + numberOfFoods + " cibi preferiti");
+Console.WriteLine($"Ecco una classifica dei miei {numberOfFoods} cibi preferiti");
 
 for (int i = 0; i < favoriteFoods.Length; i++)
 {
-    Console.WriteLine((i+1) + ") " + favoriteFoods[i]);
+    Console.WriteLine($"{i + 1}) {favoriteFoods[i]}");
+    if (i == favoriteFoods.Length - 1)
+    {
+        Console.WriteLine();
+    }
 }
-
-//se l'array è dispari, voglio stampare la posizione / 2
-var message = "Al centro: " + favoriteFoods[numberOfFoods / 2];
 
 if (favoriteFoods.Length % 2 == 0)
 {
-    message = "Al centro: " + favoriteFoods[(numberOfFoods / 2) - 1] + " e " + favoriteFoods[numberOfFoods / 2];
+    message = $"{message}{favoriteFoods[(numberOfFoods / 2) - 1]} e {favoriteFoods[numberOfFoods / 2]}";
+}
+else
+{
+    message = message + favoriteFoods[numberOfFoods / 2];
 }
 
-Console.WriteLine("Al primo posto: " + favoriteFoods[0]);
-
+Console.WriteLine($"Al primo posto: {favoriteFoods[0]}");
 Console.WriteLine(message);
-
-Console.WriteLine("In ultima posizione: " + favoriteFoods[numberOfFoods - 1]);
-
+Console.WriteLine($"In ultima posizione: {favoriteFoods[numberOfFoods - 1]}");
